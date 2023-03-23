90d3afa08465fb59f62a2c81b3353500bd9ebbd1
daveS <david.surina22@gmail.com>
2023-03-23 23:30:40 +0100

    added a basic navbar to test the scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Layout/Navbar/Navbar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
