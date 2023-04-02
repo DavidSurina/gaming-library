be7462cd6e1af00764cfc0259c14437705cbb378
daveS <david.surina22@gmail.com>
2023-04-02 21:28:45 +0200

    created basic app context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import MainLayout from "components/Layout/MainLayout/MainLayout";
 import MainRoute from "MainRoute";
+import AppContextProvider from "globals/contexts/AppContext";
 
 function App() {
   return (
-    <MainLayout>
-      <MainRoute />
-    </MainLayout>
+    <AppContextProvider>
+      <MainLayout>
+        <MainRoute />
+      </MainLayout>
+    </AppContextProvider>
   );
 }
 
--- src/globals/contexts/AppContext.tsx
+++ src/globals/contexts/AppContext.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { ReactNode, useMemo, useContext, createContext } from "react";
 
-function AppContext() {}
+type AppContextPropsType = {
+  children: ReactNode;
+};
 
-export default AppContext;
+export type AppContextType = {
+  values: any;
+};
+
+const defaultContextValue: AppContextType = {
+  values: "test",
+};
+
+export const AppContext = createContext(defaultContextValue);
+
+function AppContextProvider(props: AppContextPropsType) {
+  const { children } = props;
+  const values = "test2";
+
+  const context = useMemo((): AppContextType => {
+    return {
+      values,
+    };
+  }, [values]);
+  return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
+}
+
+export const useAppContext = (): AppContextType => useContext(AppContext);
+
+export default AppContextProvider;
