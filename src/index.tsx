e33e63b723184cc3a7dc93e7bc973b61df734147
daveS <david.surina22@gmail.com>
2023-04-02 21:03:38 +0200

    changed @import to @use

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./globals/styles/main.scss";
+import "./globals/styles/_base.scss";
+import "./globals/styles/_mixins.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
