6c8e10e0281691fe920831fa6ad38027f55a0db5
daveS <david.surina22@gmail.com>
2023-04-20 21:50:19 +0200

    small fixes

--- src/globals/hooks/useRawgApi.ts
+++ src/globals/hooks/useRawgApi.ts
@@ -3,14 +3,13 @@ import axios from "axios";
 
 function useRawgApi() {
   const baseGamingUrl = "https://api.rawg.io/api/";
-
   const currentYear = new Date().getFullYear();
   const lastYear = `${currentYear - 1}`;
   const trendingGamesUrl = `${baseGamingUrl}games?kdates=${lastYear},${currentYear}&ordering=-rating&page_size=10&key=${
     process.env.REACT_APP_GAMING_LIBRARY_API_KEY as string
   }`;
 
-  const getTrendingGames = async <T>(): Promise<T> => {
+  const getTrendingGames = async () => {
     const res = await axios.get(trendingGamesUrl);
     console.log(res);
     return res.data;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import useRawgApi from "globals/hooks/useRawgApi";
 import GameTile from "components/GameTile/GameTile";
 import { Game, GamesResults } from "globals/types/gameTypes";
+import "./style.scss";
 
 function Home() {
   const { getTrendingGames } = useRawgApi();
@@ -11,17 +12,16 @@ function Home() {
     queryKey: ["trendingGames"],
     queryFn: getTrendingGames,
   });
+  console.log(process.env.REACT_APP_GAMING_LIBRARY_API_KEY);
   if (error) return <div>Request Failed</div>;
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <section>
-      <div className="top-games_wrapper">
-        {data?.results &&
-          data.results.map((item: Game) => {
-            return <GameTile name={item?.name as string} />;
-          })}
-      </div>
+    <section className="tiles-wrapper">
+      {data?.results &&
+        data.results.map((item: Game) => {
+          return <GameTile name={item?.name as string} />;
+        })}
     </section>
   );
 }
