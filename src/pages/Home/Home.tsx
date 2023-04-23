da33fcfd6656c6aae0e61246b44b2c2e710076d1
daveS <david.surina22@gmail.com>
2023-04-23 22:20:12 +0200

    corrected home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useId } from "react";
 import { useQuery } from "@tanstack/react-query";
-import useRawgApi from "globals/hooks/useRawgApi";
 import GameTile from "components/GameTile/GameTile";
-import { Game, GamesResults } from "globals/types/gameTypes";
+import { Game, GamesResults } from "globals/types/rawgTypes";
 import "./style.scss";
+import { RawgApiService } from "globals/functions/api";
 
 function Home() {
-  const { getTrendingGames } = useRawgApi();
-
+  const id = useId();
+  const { getBestGames } = RawgApiService;
   const { data, isLoading, error } = useQuery<GamesResults>({
-    queryKey: ["trendingGames"],
-    queryFn: getTrendingGames,
+    queryKey: ["bestGames"],
+    queryFn: getBestGames,
   });
-  console.log(process.env.REACT_APP_GAMING_LIBRARY_API_KEY);
-  if (error) return <div>Request Failed</div>;
+
+  if (error) return <div>{`Request Failed - ${error}`}</div>;
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <section className="tiles-wrapper">
       {data?.results &&
         data.results.map((item: Game) => {
-          return <GameTile name={item?.name as string} />;
+          return (
+            <GameTile name={item?.name as string} key={`${id}${item.name}`} />
+          );
         })}
     </section>
   );
