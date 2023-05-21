241f8d0ab62a473dabe8aa4fbd77463906b2774a
daveS <david.surina22@gmail.com>
2023-05-21 20:33:20 +0200

    adjusted link globally

--- src/components/Layout/CustomNavbar/CustomNavbar.tsx
+++ src/components/Layout/CustomNavbar/CustomNavbar.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { useLocation, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 import { HOME_ROUTE, LIBRARY_ROUTE } from "MainRoute";
 import "./style.scss";
 
 function CustomNavbar() {
-  let location = useLocation();
   return (
     <Navbar bg="dark" sticky="top">
       <Container>
         <Navbar.Brand as={NavLink} to={HOME_ROUTE}>
           GL
         </Navbar.Brand>
-        <Nav
-          className="me-auto"
-          // activeKey={location.pathname}
-          defaultActiveKey={HOME_ROUTE}
-        >
+        <Nav className="me-auto">
           <Nav.Link as={NavLink} to={HOME_ROUTE}>
             Home
           </Nav.Link>
