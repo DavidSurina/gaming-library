f0e7ce1660e8e6a35b579c6b2c97dadfcec23051
daveS <david.surina22@gmail.com>
2023-06-01 23:07:35 +0200

    added library context

--- src/MainRoute.tsx
+++ src/MainRoute.tsx
@@ -4,8 +4,9 @@ import NotFound from "components/NotFound/NotFound";
 import GameLibrary from "pages/GameLibrary/GameLibrary";
 import Home from "pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
+import LibContextProvider from "globals/contexts/LibraryContext";
 
-export const HOME_ROUTE = "/home";
+export const HOME_ROUTE = "/";
 export const LIBRARY_ROUTE = "/lib";
 export const DETAIL_ROUTE = "/detail";
 
@@ -13,9 +14,15 @@ function MainRoute() {
   return (
     <>
       <Routes>
-        <Route index path={HOME_ROUTE} element={<Home />} />
         <Route path={HOME_ROUTE} element={<Home />} />
-        <Route path={LIBRARY_ROUTE} element={<GameLibrary />} />
+        <Route
+          path={LIBRARY_ROUTE}
+          element={
+            <LibContextProvider>
+              <GameLibrary />
+            </LibContextProvider>
+          }
+        />
         <Route path={`${DETAIL_ROUTE}/:id`} element={<GameDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/globals/contexts/LibraryContext.tsx
@@ -0,0 +1,75 @@
+import React, {
+  ReactNode,
+  useContext,
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import {
+  RawgApiService,
+  formatParams,
+  rawgSubUrls,
+} from "globals/functions/api";
+import { rawgParams } from "globals/rawgParams";
+import { GameParams, GamesResults } from "globals/types/rawgTypes";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+type LibContextPropType = {
+  children: JSX.Element;
+};
+
+type QueryParams = {
+  queryKey: string;
+  params: string;
+};
+
+export type LibContextType = {
+  initialUrl: string;
+  subUrl: string;
+  setSubUrl: Dispatch<SetStateAction<string>>;
+  queryParams: QueryParams;
+  setQueryParams: Dispatch<SetStateAction<QueryParams>>;
+};
+
+const defaultContextValue: LibContextType = {
+  initialUrl: ``,
+  subUrl: rawgSubUrls.game,
+  setSubUrl: () => {},
+  queryParams: {
+    queryKey: "",
+    params: "",
+  },
+  setQueryParams: () => {},
+};
+
+export const LibContext = createContext(defaultContextValue);
+
+function LibContextProvider(props: LibContextPropType) {
+  const { children } = props;
+
+  const [subUrl, setSubUrl] = useState(rawgSubUrls.game);
+  const [queryParams, setQueryParams] = useState({
+    queryKey: "bestGames",
+    params: formatParams(rawgParams["bestGames"]),
+  });
+
+  const initialUrl = `${subUrl}?${queryParams.params}`;
+
+  const context = (): LibContextType => {
+    return {
+      queryParams,
+      setQueryParams,
+      subUrl,
+      setSubUrl,
+      initialUrl,
+    };
+  };
+  return (
+    <LibContext.Provider value={context()}>{children}</LibContext.Provider>
+  );
+}
+
+export const useLibContext = (): LibContextType => useContext(LibContext);
+
+export default LibContextProvider;
