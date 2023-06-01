f39fa194b3a610b78445208b3b7a37ca85c0164b
daveS <david.surina22@gmail.com>
2023-06-01 23:10:27 +0200

    changed some library logic

--- src/pages/GameLibrary/GameLibrary.tsx
+++ src/pages/GameLibrary/GameLibrary.tsx
@@ -1,36 +1,43 @@
-import React, { useId, useRef, useEffect, useState } from "react";
+import React, { useId, useRef, useEffect, ChangeEvent } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useLibContext } from "globals/contexts/LibraryContext";
 
 import { Form } from "react-bootstrap";
 import GameTile from "components/GameTile/GameTile";
 import LoadingSpinner from "components/LoadingSpinner/LoadingSpinner";
+import SearchInput from "components/SearchInput/SearchInput";
 
-import {
-  RawgApiService,
-  formatParams,
-  rawgSubUrls,
-} from "globals/functions/api";
-import { Game, GameParams, GamesResults } from "globals/types/rawgTypes";
-import "./style.scss";
+import { RawgApiService, formatParams } from "globals/functions/api";
+import { Game, GamesResults } from "globals/types/rawgTypes";
 import { rawgParams } from "globals/rawgParams";
+import "./style.scss";
 
 function GameLibrary() {
   const id = useId();
   const gameRef = useRef<HTMLDivElement>(null);
   const { getRawgData } = RawgApiService;
-  const [selectValue, setSelectValue] = useState("bestGames");
-  console.log(selectValue);
-  const initialUrl = `${rawgSubUrls.game}?${formatParams(
-    rawgParams[`${selectValue}` as keyof typeof rawgParams]
-  )}`;
+  const { queryParams, setQueryParams, initialUrl } = useLibContext();
+
   const { data, isLoading, error, fetchNextPage, isFetching, hasNextPage } =
     useInfiniteQuery<GamesResults>({
-      queryKey: [selectValue, initialUrl],
+      queryKey: [queryParams.queryKey, initialUrl],
       queryFn: ({ pageParam = initialUrl }) => getRawgData(pageParam),
       getNextPageParam: (lastPage) => {
         return lastPage.next;
       },
     });
+
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const formattedParams = formatParams(
+      rawgParams[`${e.target.value}` as keyof typeof rawgParams]
+    );
+
+    setQueryParams((prevState) => ({
+      ...prevState,
+      queryKey: e.target.value,
+      params: formattedParams,
+    }));
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -57,35 +64,39 @@ function GameLibrary() {
     };
   }, [gameRef.current, isLoading, hasNextPage]);
 
-  if (isLoading) return <LoadingSpinner />;
   if (error) return <div>{`Request Failed - ${error}`}</div>;
-
+  console.log(data);
   return (
     <section>
       <div className="filtering-wrapper">
         <Form.Select
           size="lg"
-          style={{ width: "50%", margin: "1rem" }}
-          onChange={(e) => setSelectValue(e.target.value)}
+          className="m-3"
+          style={{ width: "45%" }}
+          onChange={handleSelect}
         >
           <option value="bestGames">Best Games</option>
           <option value="latestReleases">Latest Releases</option>
         </Form.Select>
+        <SearchInput />
       </div>
-      {data?.pages && (
+      {!isLoading && data?.pages && (
         <div className="tiles-wrapper">
-          {data.pages.map((group: GamesResults, dataIndex) => {
-            return group.results.map((game: Game, resultIndex) => {
+          {data.pages.map((group: GamesResults) => {
+            return group.results.map((game: Game) => {
               return <GameTile game={game} key={`${id}${game.name}`} />;
             });
           })}
+          {data?.pages[0].count === 0 && (
+            <div style={{ textAlign: "center" }}>No Results</div>
+          )}
+          {!hasNextPage && !isFetching && data?.pages[0]?.count > 0 && (
+            <div style={{ textAlign: "center" }}>No more entries</div>
+          )}
         </div>
       )}
       {data && <span ref={gameRef} />}
       {isFetching && <LoadingSpinner />}
-      {!hasNextPage && !isFetching && (
-        <div style={{ textAlign: "center" }}>No more entries</div>
-      )}
     </section>
   );
 }
