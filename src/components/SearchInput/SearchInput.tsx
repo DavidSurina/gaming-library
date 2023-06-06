90dd84bd43b63976d40e87af6d1e51242a7bd624
daveS <david.surina22@gmail.com>
2023-06-06 21:49:02 +0200

    changed the search to be a form

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {FormEvent, useState} from "react";
 import { Button, Form, InputGroup, FloatingLabel } from "react-bootstrap";
 
 import { useLibContext } from "globals/contexts/LibraryContext";
@@ -7,8 +7,8 @@ function SearchInput() {
   const { setCurrentQuery } = useLibContext();
   const [input, setInput] = useState("");
 
-  const handleClick = () => {
-    console.log("fired");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setCurrentQuery((prevState) => ({
       ...prevState,
       queryKey: `gameSearch=${input}`,
@@ -17,24 +17,25 @@ function SearchInput() {
   };
 
   return (
-    <InputGroup className="p-3" style={{ flexGrow: 1 }}>
-      <FloatingLabel controlId="floatingInput" label="Find games">
-        <Form.Control
-          type="input"
-          placeholder="abcde"
-          value={input}
-          onInput={(e) => setInput(e.currentTarget.value)}
-        />
-      </FloatingLabel>
-      <Button
-        type="button"
-        variant="outline-secondary"
-        onClick={handleClick}
-        disabled={input.length === 0}
-      >
-        Search
-      </Button>
-    </InputGroup>
+      <Form onSubmit={(e) => handleSubmit(e)} style={{ flexGrow: 1 }}>
+        <InputGroup className="p-3" >
+          <FloatingLabel controlId="floatingInput" label="Find games">
+            <Form.Control
+              type="input"
+              placeholder="abcde"
+              value={input}
+              onInput={(e) => setInput(e.currentTarget.value)}
+            />
+          </FloatingLabel>
+          <Button
+            type="submit"
+            variant="outline-secondary"
+            disabled={input.length === 0}
+          >
+            Search
+          </Button>
+        </InputGroup>
+      </Form>
   );
 }
 
