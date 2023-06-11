584aa1a73981030e3bd619eaa85ce48c0a7053cf
daveS <david.surina22@gmail.com>
2023-06-11 18:17:56 +0200

    adjusted style of gameTile and added more info

--- src/components/GameTile/GameTile.tsx
+++ src/components/GameTile/GameTile.tsx
@@ -12,7 +12,7 @@ type PropTypes = {
 
 function GameTile(props: PropTypes) {
   const {
-    game: { name, id, background_image },
+    game: { name, id, background_image, platforms, released, metacritic },
   } = props;
 
   return (
@@ -21,8 +21,22 @@ function GameTile(props: PropTypes) {
         <div className="img-container">
           <Image src={background_image} alt="game-img" />
         </div>
+        <Card.Title>{name}</Card.Title>
         <Card.Body>
-          <Card.Title>{name}</Card.Title>
+          <div className="d-flex justify-content-between align-items-center py-1">
+            <Card.Subtitle>Released:</Card.Subtitle>
+            <Card.Text>{new Date(released).toLocaleDateString()}</Card.Text>
+          </div>
+          <div className="d-flex justify-content-between align-items-center py-1">
+            <Card.Subtitle>Critic rating:</Card.Subtitle>
+            <Card.Text>{metacritic}/100</Card.Text>
+          </div>
+          <div className="d-flex flex-column justify-content-between py-1">
+            <Card.Subtitle>Platforms:</Card.Subtitle>
+            <Card.Text>
+              {platforms.map((p, index) => p.platform.name).join(", ")}
+            </Card.Text>
+          </div>
         </Card.Body>
       </Card>
     </Link>
