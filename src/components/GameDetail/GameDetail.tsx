1cfc8da3e60cf530ab265f5f582bed2f09a9aa3c
daveS <david.surina22@gmail.com>
2023-06-12 15:58:18 +0200

    adjusted GameDetail

--- src/components/GameDetail/GameDetail.tsx
+++ src/components/GameDetail/GameDetail.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { RawgApiService, rawgSubUrls } from "../../globals/functions/api";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { Button } from "react-bootstrap";
 
 function GameDetail() {
   const { id } = useParams();
-  // TODO: add call to get the game data
+  const navigate = useNavigate();
+  const { getRawgData } = RawgApiService;
+  const param = `${rawgSubUrls.game}/${id}`;
+  console.log(id);
+  // TODO different type result on getGame query - create 2 fn getSingle/getMultiple ?
+  const { data, isLoading, error, isInitialLoading } = useQuery({
+    queryKey: [`game-${id}`],
+    queryFn: () => getRawgData(param, {}),
+  });
+
+  if (isInitialLoading) {
+    return <LoadingSpinner />;
+  }
+  console.log(data);
 
   return (
-    <section>
-      <h1>{id} Game</h1>
+    <section style={{ width: "100%" }}>
+      <Button type="button" onClick={() => navigate(-1)} style={{}}>
+        Back
+      </Button>
+      <div style={{ width: "100%", overflow: "hidden", maxHeight: "60vh" }}>
+        <img
+          src={data?.background_image}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
     </section>
   );
 }
