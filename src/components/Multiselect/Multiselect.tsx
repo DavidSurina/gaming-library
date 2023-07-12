bec654cd919817696cf9af0c9668c0c75f5823a7
daveS <david.surina22@gmail.com>
2023-07-12 17:33:17 +0200

    initial multiselect commit

---
+++ src/components/Multiselect/Multiselect.tsx
@@ -0,0 +1,106 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { useSelect, UseSelectProps } from "downshift";
+import { CurrentQueryType } from "../../globals/contexts/LibraryContext";
+import { ChevronDown, ChevronUp } from "react-bootstrap-icons";
+import clsx from "clsx";
+import { FilteringParamsType } from "../FilterMenu/FilterMenu";
+
+const selectWidth = "330px"; // TODO move out globally or separate file
+
+interface ExtendedSelectProps extends UseSelectProps<CurrentQueryType> {
+  selectedItems: FilteringParamsType;
+  setSelectedItems: Dispatch<SetStateAction<FilteringParamsType>>;
+}
+function Multiselect(props: ExtendedSelectProps) {
+  const {
+    labelId,
+    selectedItems: wholeSelectedItems,
+    setSelectedItems,
+    ...rest
+  } = props;
+  const selectedItems = wholeSelectedItems[labelId + ""];
+
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+    getLabelProps,
+  } = useSelect({
+    ...rest,
+    labelId: labelId,
+    onSelectedItemChange: ({ selectedItem }) => {
+      if (!selectedItem) {
+        return;
+      }
+
+      const index = selectedItems.indexOf(selectedItem);
+
+      if (index > 0) {
+        setSelectedItems([
+          ...selectedItems.slice(0, index),
+          ...selectedItems.slice(index + 1),
+        ]);
+      } else if (index === 0) {
+        setSelectedItems([...selectedItems.slice(1)]);
+      } else {
+        setSelectedItems([...selectedItems, selectedItem]);
+      }
+    },
+  });
+
+  return (
+    <div
+      className="w-20 m-auto justify-content-center"
+      style={{ zIndex: "auto" }}
+    >
+      <div
+        className="p-3 bg-body d-inline-flex justify-content-between pointer-event border border-white rounded-2"
+        {...getToggleButtonProps()}
+        style={{ width: selectWidth }}
+      >
+        {/* TODO Selected item count */}
+        <span>Selected item count</span>
+        <span className="px-2">{isOpen ? <ChevronUp /> : <ChevronDown />}</span>
+      </div>
+      <ul
+        className={clsx(
+          "position-absolute bg-body mt-1 shadow-sm overflow-auto p-0 border border-white",
+          {
+            "hidden border-0": !isOpen,
+          }
+        )}
+        style={{ width: selectWidth, zIndex: "auto", maxHeight: "400px" }}
+        {...getMenuProps()}
+      >
+        {isOpen &&
+          props.items.map((item, index) => {
+            return (
+              <li
+                style={isOpen && { zIndex: "auto" }}
+                className={clsx("py-2 px-3 shadow-sm flex-col", {
+                  "fw-bold": selectedItem === item,
+                  "bg-secondary": highlightedIndex === index,
+                })}
+                key={`${item.queryKey}${index}`}
+                {...getItemProps({ item, index })}
+              >
+                <input
+                  type="checkbox"
+                  className="p-2 h-5 w-5"
+                  checked={selectedItems.some(
+                    (i) => i.queryKey === item.queryKey
+                  )}
+                  value={item.queryKey}
+                />
+                <span>{item.queryKey}</span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default Multiselect;
