78bfb1f0768384da7806baecda2963bb44eb6616
daveS <david.surina22@gmail.com>
2023-07-18 19:46:12 +0200

    added handle change fn

--- src/components/FilterSlider/FilterSlider.tsx
+++ src/components/FilterSlider/FilterSlider.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction } from "react";
 import ReactSlider from "react-slider";
 import { FilteringParamsType } from "../FilterMenu/FilterMenu";
 import "./style.scss";
+import { CurrentQueryType } from "../../globals/contexts/LibraryContext";
 
 type PropTypes = {
   state: FilteringParamsType;
@@ -9,13 +10,18 @@ type PropTypes = {
 };
 function FilterSlider(props: PropTypes) {
   const { state, setState } = props;
+  console.log(state);
+  function handleChange(value: number[]) {
+    const param = [{ queryKey: "metacritic", params: value.join(",") }];
+    setState({ ...state, metacritic: param });
+  }
 
   return (
     <ReactSlider
       className="slider-wrapper"
       thumbClassName="slider-thumb"
       trackClassName="slider-track"
-      onChange={(value, index) => console.log(value, index)}
+      onChange={handleChange}
       min={0}
       max={100}
       defaultValue={[0, 100]}
