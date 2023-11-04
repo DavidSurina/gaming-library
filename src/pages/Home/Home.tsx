9290ce0e22b39c54e51d9a600fee17759054bc27
daveS <david.surina22@gmail.com>
2023-11-04 15:53:09 +0100

    first rss render

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import NewsTile from "../../components/NewsTile/NewsTile";
+import axios from "axios";
+
+export type ParsedFeedType = {
+    link: string;
+    title: string;
+    author: string;
+}
+
+const CORS_PROXY = 'https://corsproxy.io/?';
+const gamespotUrl = 'https://www.gamespot.com/feeds/game-news';
 
 function Home() {
-  return (
-    <Container fluid="true" className="vh-100">
-      <Row>
-        <h1>Home</h1>
-      </Row>
-    </Container>
-  );
+    const [loading, setLoading] = useState(false);
+    const [feed, setFeed] = useState<ParsedFeedType[]>([]);
+
+    const getRss = async () => {
+        await axios({
+            method: 'get',
+            url: CORS_PROXY + gamespotUrl,
+            withCredentials: false,
+            headers: {
+                "Content-Type": 'text/plain',
+            }
+        }).then((res) => {
+            // const {contents} = res.data;
+            // console.log(jsonData);
+            const feed = new window.DOMParser().parseFromString(res.data, "text/xml");
+            const items = feed.querySelectorAll("item");
+
+            const feedItems = [...items].map((el) => ({
+                link: el.querySelector("link")?.innerHTML,
+                title: el.querySelector("title")?.innerHTML,
+                author: el.querySelector("creator")?.innerHTML,
+            }));
+            console.log(res)
+            console.log(feed)
+            console.log(items)
+            setFeed(feedItems);
+            setFeed(feedItems);
+        })
+    };
+
+    useEffect(() => {
+        setLoading(true)
+        getRss();
+        setLoading(false);
+    }, []);
+
+    return (
+        <Container fluid="true" className="vh-100">
+            {loading && <LoadingSpinner/>}
+            {feed && feed.map((item) => <NewsTile key={`${item.title}tile`} data={item}/>)}
+        </Container>
+    );
 }
 
 export default Home;
