94b41bd0f6daa3e6022c7378b2d1dd7ba00a8bc1
daveS <david.surina22@gmail.com>
2023-11-08 18:47:52 +0100

    adjusted style of newsTile

--- src/components/NewsTile/NewsTile.tsx
+++ src/components/NewsTile/NewsTile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {ParsedFeedType} from "../../pages/Home/Home";
+import './style.scss';
 
 type PropTypes = {
     data: ParsedFeedType;
@@ -12,11 +13,11 @@ function NewsTile(props: PropTypes) {
     const {link, title, author} = data;
 
     return (
-        <Card>
+        <Card className="news-tile">
             <h2>{title}</h2>
             <h4>{author}</h4>
             <div>{}</div>
-            <Link to={link}>Go to</Link>
+            <Link to={link} target="_blank">Go to</Link>
         </Card>
     );
 }
