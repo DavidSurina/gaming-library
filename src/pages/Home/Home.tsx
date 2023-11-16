cefca43396958df2c3adaf911eca8bbdb050612c
daveS <david.surina22@gmail.com>
2023-11-16 14:31:09 +0100

    added hook for RssFeed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,61 +2,41 @@ import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import NewsTile from "../../components/NewsTile/NewsTile";
-import axios from "axios";
 import './style.scss';
+import useRssFeed from "../../globals/hooks/useRssFeed";
 
 export type ParsedFeedType = {
-    link: string;
-    title: string;
-    author: string;
+    link: string | undefined;
+    title: string | undefined;
+    author: string | undefined;
+    pubDate: string | undefined;
+    description: string | undefined;
+    img: string | null | undefined;
 }
 
-const CORS_PROXY = 'https://corsproxy.io/?';
-const gamespotUrl = 'https://www.gamespot.com/feeds/game-news';
-
 function Home() {
-    const [loading, setLoading] = useState(false);
+    const {getRss, loading} = useRssFeed();
     const [feed, setFeed] = useState<ParsedFeedType[]>([]);
 
-    const getRss = async () => {
-        await axios({
-            method: 'get',
-            url: CORS_PROXY + gamespotUrl,
-            withCredentials: false,
-            headers: {
-                "Content-Type": 'text/plain',
-            }
-        }).then((res) => {
-            const feed = new window.DOMParser().parseFromString(res.data, "text/xml");
-            const items = feed.querySelectorAll("item");
-
-            const feedItems = [...items].map((el) => ({
-                link: el.querySelector("link")?.innerHTML,
-                title: el.querySelector("title")?.innerHTML,
-                author: el.querySelector("creator")?.innerHTML,
-            }));
-            console.log(res)
-            console.log(feed)
-            console.log(items)
-            setFeed(feedItems);
-            setFeed(feedItems);
-        })
-    };
+    //TODO maybe create a hook out of all this - to get feed/get next page of it/ just to hold most of the logic
+    //TODO maybe do it same way as the game library - tanstack infinite query - caching will be easier
 
     useEffect(() => {
-        setLoading(true)
-        getRss();
-        setLoading(false);
+        getRss().then(res => {
+            if (res) {
+                setFeed(res);
+            }
+        })
     }, []);
 
     return (
         <Container fluid="true" className="news-container">
-            {loading && <LoadingSpinner/>}
             <section className="left-col"></section>
             <section className="middle-col">
                 {feed && feed.map((item) => <NewsTile key={`${item.title}tile`} data={item}/>)}
             </section>
             <section className="right-col"></section>
+            {loading && <LoadingSpinner/>}
         </Container>
     );
 }
