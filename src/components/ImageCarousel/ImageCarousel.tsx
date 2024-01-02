94c43881d358ffbe61c8763cbe5b7f8027046547
daveS <david.surina22@gmail.com>
2024-01-02 18:37:27 +0100

    added carousel and modal

---
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,91 @@
+import React, {MutableRefObject, useCallback, useRef, useState} from "react";
+import {GameScreenshotResultsType} from "../../globals/types/rawgTypes";
+import {Button, Carousel, Image, Modal} from "react-bootstrap";
+import {
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon,
+} from "react-bootstrap-icons";
+import './style.scss';
+
+type PropTypes = {
+    images: GameScreenshotResultsType;
+}
+
+function ImageCarousel(props: PropTypes) {
+    const {images} = props;
+    const [galleryOpen, setGalleryOpen] = useState(false);
+    const [imageIndex, setImageIndex] = useState<number>(0);
+
+    const scrollWrapperRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+    const openGalleryHandle = useCallback((index: number) => {
+        setGalleryOpen(true);
+        setImageIndex(index);
+    }, []);
+
+    // const increaseSelectedIndex = useCallback(() => {
+    //     console.log('clicked')
+    //     console.log(imageIndex, images.results.length)
+    //     if (imageIndex < images.results.length) {
+    //         setImageIndex(prevState => ++prevState);
+    //     }
+    // }, [imageIndex, images.results]);
+    //
+    // const decreaseSelectedIndex = useCallback(() => {
+    //     if (imageIndex > 1) {
+    //         console.log('clicked')
+    //         setImageIndex(prevState => prevState - 1);
+    //     }
+    // }, [imageIndex]);
+
+    const scroll = (offset: number) => {
+        if (scrollWrapperRef.current) {
+            console.log(offset, scrollWrapperRef.current?.scrollLeft)
+            scrollWrapperRef.current.scrollLeft = scrollWrapperRef.current?.scrollLeft + offset;
+        }
+    }
+
+    const handleImageIndex = (selectedIndex: number) => {
+        setImageIndex(selectedIndex);
+    };
+
+    const resetGallery = useCallback(() => {
+        setGalleryOpen(false);
+        setImageIndex(0);
+    }, []);
+
+    return <>
+        <div className="carousel" ref={scrollWrapperRef}>
+            <Button onClick={() => scroll(-50)} className="carousel_btn-left"><ChevronLeftIcon/></Button>
+            <div className="carousel_wrapper">
+                {
+                    images.results.map((result, index) => (
+                        <div key={result.id} className="carousel_img-wrapper"
+                             onClick={() => openGalleryHandle(index)}>
+                            <Image src={result.image} className="carousel_img"/>
+                        </div>
+                    ))
+                }
+            </div>
+            <Button onClick={() => scroll(-50)} className="carousel_btn-right"><ChevronRightIcon/></Button>
+        </div>
+        <Modal show={galleryOpen} onHide={resetGallery} fullscreen>
+            <Modal.Header closeButton/>
+            <Modal.Body>
+                <Carousel fade activeIndex={imageIndex} onSelect={handleImageIndex} className="modal_gallery-wrapper">
+                    {
+                        images.results.map((item) => {
+                            return <Carousel.Item key={`carouselItem${item.id}`}>
+                                <div className="modal_gallery-img">
+                                    <Image className="modal_gallery-img" src={item.image}/>
+                                </div>
+                            </Carousel.Item>
+                        })
+                    }
+                </Carousel>
+            </Modal.Body>
+        </Modal>
+    </>
+}
+
+export default ImageCarousel;
