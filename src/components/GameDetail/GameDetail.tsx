b2ff52d9565cf944ac15b8c440e105b2309ffa95
daveS <david.surina22@gmail.com>
2024-01-06 14:36:11 +0100

    formatting

--- src/components/GameDetail/GameDetail.tsx
+++ src/components/GameDetail/GameDetail.tsx
@@ -1,91 +1,86 @@
 import React from "react";
-import {useParams, useNavigate} from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
-import {RawgApiService, rawgSubUrls} from "../../globals/functions/rawgApi";
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { RawgApiService, rawgSubUrls } from "../../globals/functions/rawgApi";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
-import {Button, Carousel, Image, Tab, Tabs} from "react-bootstrap";
-import {Game, GameScreenshotResults} from "../../globals/types/rawgTypes";
-import {Game, GameScreenshotResultsType} from "../../globals/types/rawgTypes";
-import './style.scss';
+import { Button, Tab, Tabs } from "react-bootstrap";
+import { ArrowLeft } from "react-bootstrap-icons";
+import { Game, GameScreenshotResultsType } from "../../globals/types/rawgTypes";
+import "./style.scss";
+import ImageCarousel from "../ImageCarousel/ImageCarousel";
 
 function GameDetail() {
-    const {id} = useParams();
-    const navigate = useNavigate();
-    const {getRawgData} = RawgApiService;
-    const gameParam = `${rawgSubUrls.game}/${id}`;
-    const screenShotParam = `${rawgSubUrls.game}/${id}/screenshots`;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { getRawgData } = RawgApiService;
+  const gameParam = `${rawgSubUrls.game}/${id}`;
+  const screenShotParam = `${rawgSubUrls.game}/${id}/screenshots`;
 
-    const {data, isInitialLoading} = useQuery<Game>({
-        queryKey: [`game-${id}`],
-        queryFn: () => getRawgData<Game>(gameParam, {}),
+  const { data, isInitialLoading } = useQuery<Game>({
+    queryKey: [`game-${id}`],
+    queryFn: () => getRawgData<Game>(gameParam, {}),
+  });
+
+  const { data: screenshotsData, isInitialLoading: screenshotsInitialLoading } =
+    useQuery({
+      queryKey: [``],
+      queryFn: () =>
+        getRawgData<GameScreenshotResultsType>(screenShotParam, {}),
     });
 
-    const {data: screenshotsData, isInitialLoading: screenshotsInitialLoading} = useQuery({
-        queryKey: [``],
-        queryFn: () => getRawgData<GameScreenshotResults>(screenShotParam, {}),
-    })
+  if (isInitialLoading || screenshotsInitialLoading) {
+    return <LoadingSpinner />;
+  }
 
-    if (isInitialLoading || screenshotsInitialLoading) {
-        return <LoadingSpinner/>;
-    }
+  return (
+    <section
+      className="game-detail_wrapper"
+      style={{
+        backgroundImage: `url(${data?.background_image})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="game-detail_top-section">
+        <div className="game-detail_top-content">
+          <Button
+            type="button"
+            className="game-detail_back-btn"
+            onClick={() => navigate(-1)}
+          >
+            <ArrowLeft size={25} />
+            <span>Back</span>
+          </Button>
+          <h1 className="game-detail_main-heading">{data?.name}</h1>
+          {screenshotsData && <ImageCarousel images={screenshotsData} />}
+        </div>
+      </div>
 
-
-    return (
-        <section className="game-detail_wrapper"
-                 style={{
-                     backgroundImage: `url(${data?.background_image})`,
-                     backgroundPosition: 'center',
-                     backgroundSize: 'cover'
-                 }}>
-
-            <div className="game-detail_top-section">
-                <Button type="button" variant='link' className="game-detail_back-btn" onClick={() => navigate(-1)}>
-                    Back
-                </Button>
-                <h1 className="game-detail_main-heading">{data?.name}</h1>
-                <div className="game-detail_top-content">
-                    {screenshotsData &&
-                        <Carousel className="game-detail_carousel">
-                            {screenshotsData.results.map((item) => {
-                                return <Carousel.Item key={`${item.id}Slide`}>
-                                    <div className="game-detail_carousel-img-container">
-                                        <Image className="game-detail_carousel-img" src={item.image}/>
-                                    </div>
-                                </Carousel.Item>
-                            })}
-                        </Carousel>
-                    }
-                    <ul className="game-detail_top-description">
-                        <li className="game-detail_description-row">
-                            <span>Publisher:</span>
-                            <span></span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="game-detail_bottom-section">
-                <Tabs defaultActiveKey="details">
-                    <Tab eventKey="details" title='Details'>
-                        <section>
-                            <div>Hi tab 1</div>
-
-                        </section>
-                    </Tab>
-                    <Tab eventKey='reviews' title='Reviews'>
-                        <section>
-                            Hi tab 2
-                        </section>
-                    </Tab>
-                    <Tab eventKey='more' title='More'>
-                        <section>
-                            Hi tab 3
-                        </section>
-                    </Tab>
-                </Tabs>
-            </div>
-        </section>
-    );
+      <div className="game-detail_bottom-section">
+        <Tabs defaultActiveKey="details" className="game-detail_tabs-wrapper">
+          <Tab
+            eventKey="details"
+            title="Details"
+            className="game-detail_tab-wrapper"
+          >
+            <section>
+              <span>{`" ${data?.description_raw} "`}</span>
+            </section>
+          </Tab>
+          <Tab
+            eventKey="reviews"
+            title="Reviews"
+            className="game-detail_tab-wrapper"
+          >
+            <section>Hi tab 2</section>
+          </Tab>
+          <Tab eventKey="more" title="More" className="game-detail_tab-wrapper">
+            <section>Hi tab 3</section>
+          </Tab>
+        </Tabs>
+      </div>
+    </section>
+  );
 }
 
 export default GameDetail;
