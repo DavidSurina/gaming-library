f76f1c7b3c00fd5607be9d214974583e32916f5f
daveS <david.surina22@gmail.com>
2024-01-31 18:38:38 +0100

    improved grid on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,57 +1,54 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import useRssFeed from "../../globals/hooks/useRssFeed";
 import NewsTile from "../../components/NewsTile/NewsTile";
-import './style.scss';
+import "./style.scss";
 
 function Home() {
-    const {data, isLoading, fetchNextPage, isFetching, hasNextPage} = useRssFeed();
-    const colEndRef = useRef(null);
+  const { data, isLoading, fetchNextPage, isFetching, hasNextPage } =
+    useRssFeed();
+  const colEndRef = useRef(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    if (!hasNextPage) return;
-                    if (entry.isIntersecting) {
-                        // console.log("gets fetched");
-                        fetchNextPage();
-                    }
-                });
-            },
-            {
-                rootMargin: "0px 0px 400px 0px",
-            }
-        );
-
-        if (colEndRef.current && hasNextPage) {
-            observer.observe(colEndRef.current);
-        }
-
-        return () => {
-            observer.disconnect();
-        };
-    }, [isLoading, hasNextPage]);
-
-    return (
-        <Container fluid="true" className="news-container">
-            <section className="left-col"></section>
-
-            <section className="middle-col">
-                {data?.pages &&
-                    data.pages.map(pages => {
-                        return pages.feedItems.map((item) => {
-                            return <NewsTile key={`${item.title}tile`} data={item}/>
-                        })
-                    })}
-                {data && <span ref={colEndRef}/>}
-                {isLoading || isFetching && <LoadingSpinner/>}
-            </section>
-
-            <section className="right-col"></section>
-        </Container>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (!hasNextPage) return;
+          if (entry.isIntersecting) {
+            // console.log("gets fetched");
+            fetchNextPage();
+          }
+        });
+      },
+      {
+        rootMargin: "0px 0px 400px 0px",
+      },
     );
+
+    if (colEndRef.current && hasNextPage) {
+      observer.observe(colEndRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [isLoading, hasNextPage]);
+
+  return (
+    <>
+      <Container fluid="true" className="news-container">
+        {data?.pages &&
+          data.pages.map((pages) => {
+            return pages.feedItems.map((item) => {
+              return <NewsTile key={`${item.title}tile`} data={item} />;
+            });
+          })}
+        {isLoading || (isFetching && <LoadingSpinner />)}
+        {data && <span ref={colEndRef} />}
+      </Container>
+    </>
+  );
 }
 
 export default Home;
