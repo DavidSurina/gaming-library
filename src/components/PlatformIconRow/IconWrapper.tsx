e99af6bdb2f6a47b4b5d0fd8db191816021b73b5
daveS <david.surina22@gmail.com>
2024-02-02 17:27:47 +0100

    adjusted IconWrapper

--- src/components/PlatformIconRow/IconWrapper.tsx
+++ src/components/PlatformIconRow/IconWrapper.tsx
@@ -2,24 +2,11 @@ import React from "react";
 
 type PropTypes = {
   icon: JSX.Element;
-  bgColor?: string;
 };
 
-IconWrapper.defaultProps = {
-  bgColor: "#3e8df7",
-};
 function IconWrapper(props: PropTypes) {
-  const { icon, bgColor } = props;
-  return (
-    <li
-      className="mx-1 p-1 rounded-circle"
-      style={{
-        backgroundColor: bgColor,
-      }}
-    >
-      {icon}
-    </li>
-  );
+  const { icon } = props;
+  return <li className="mx-1">{icon}</li>;
 }
 
 export default IconWrapper;
