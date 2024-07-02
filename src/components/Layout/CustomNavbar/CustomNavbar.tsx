086d71abda4aa2a407c3cdff16c8600db46b45a3
daveS <david.surina22@gmail.com>
2024-07-02 15:56:28 +0200

    different transition for nav

--- src/components/Layout/CustomNavbar/CustomNavbar.tsx
+++ src/components/Layout/CustomNavbar/CustomNavbar.tsx
@@ -1,31 +1,31 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
-import {Navbar, Nav, Container} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
-import {HOME_ROUTE, LIBRARY_ROUTE} from "MainRoute";
+import { HOME_ROUTE, LIBRARY_ROUTE } from "MainRoute";
 import "./style.scss";
 
 function CustomNavbar() {
-    return (
-        <Navbar bg="dark" sticky="top">
-            <Container>
-                <Navbar.Brand as={NavLink} to={HOME_ROUTE}>
-                    <span>G</span>
-                    <span>ame</span>
-                    <span>L</span>
-                    <span>ibrary</span>
-                </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={NavLink} to={HOME_ROUTE}>
-                        Home
-                    </Nav.Link>
-                    <Nav.Link as={NavLink} to={`${LIBRARY_ROUTE}`}>
-                        Library
-                    </Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar sticky="top">
+      <Container>
+        <Navbar.Brand as={NavLink} to={HOME_ROUTE}>
+          <span>G</span>
+          <span>ame</span>
+          <span>L</span>
+          <span>ibrary</span>
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to={HOME_ROUTE}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={`${LIBRARY_ROUTE}`}>
+            Library
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default CustomNavbar;
