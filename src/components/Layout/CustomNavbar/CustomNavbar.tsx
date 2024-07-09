a363ddfa60b5fa8180a6c04667a589eee461f324
daveS <david.surina22@gmail.com>
2024-07-09 18:26:21 +0200

    changes to customNavbar

--- src/components/Layout/CustomNavbar/CustomNavbar.tsx
+++ src/components/Layout/CustomNavbar/CustomNavbar.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Controller as ControllerIcon } from "react-bootstrap-icons";
 
+import NavSearch from "../../NavSearch/NavSearch";
 import { HOME_ROUTE, LIBRARY_ROUTE } from "MainRoute";
 import "./style.scss";
 
 function CustomNavbar() {
   return (
     <Navbar sticky="top">
-      <Navbar.Brand as={NavLink} to={HOME_ROUTE}>
-        <ControllerIcon size={40} />
-        <span>G</span>
-        <span>ame</span>
-        <span>L</span>
-        <span>ibrary</span>
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link as={NavLink} to={HOME_ROUTE}>
-          Home
-        </Nav.Link>
-        <Nav.Link as={NavLink} to={`${LIBRARY_ROUTE}`}>
-          Library
-        </Nav.Link>
-      </Nav>
+      <Container>
+        <Nav>
+          <Navbar.Brand as={NavLink} to={HOME_ROUTE}>
+            <ControllerIcon size={40} />
+            <span>G</span>
+            <span>ame</span>
+            <span>L</span>
+            <span>ibrary</span>
+          </Navbar.Brand>
+
+          <Nav.Link as={NavLink} to={HOME_ROUTE}>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={`${LIBRARY_ROUTE}`}>
+            Library
+          </Nav.Link>
+        </Nav>
+        <NavSearch />
+      </Container>
     </Navbar>
   );
 }
