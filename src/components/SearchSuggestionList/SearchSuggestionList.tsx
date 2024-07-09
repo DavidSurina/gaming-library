e34111f5743dbcfe41267e7e020077fc4f17f46d
daveS <david.surina22@gmail.com>
2024-07-09 18:25:43 +0200

    adjustments to searchSuggestions

--- src/components/SearchSuggestionList/SearchSuggestionList.tsx
+++ src/components/SearchSuggestionList/SearchSuggestionList.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { Game } from "../../globals/types/rawgTypes";
+import { useNavigate } from "react-router-dom";
+import { Badge, Button, ListGroup, ListGroupItem } from "react-bootstrap";
+import { ChevronRight } from "react-bootstrap-icons";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { GamesResultsType } from "../../globals/types/rawgTypes";
+import { LIBRARY_ROUTE } from "../../MainRoute";
 import "./style.scss";
 
 type PropTypes = {
-  listData: Array<Game>;
+  data: GamesResultsType | undefined;
   loading: boolean;
 };
 
 function SearchSuggestionList(props: PropTypes) {
-  const { listData, loading } = props;
+  const { data, loading } = props;
+  const navigate = useNavigate();
 
   if (loading)
     return (
@@ -19,23 +23,53 @@ function SearchSuggestionList(props: PropTypes) {
       </div>
     );
 
+  if (!data || data.results.length === 0) {
+    return (
+      <div className="suggestion_wrapper">
+        <span className="text_wrapper">No data</span>
+      </div>
+    );
+  }
+
+  const handleListItemClick = (slug: string) => {
+    navigate(`${LIBRARY_ROUTE}/${slug}`);
+  };
+
+  // TODO add other image if one from data unavailable
   return (
     <ListGroup className="suggestion_wrapper">
-      {listData && listData.length ? (
-        listData.map((game) => (
-          <ListGroupItem key={`${game.name}Suggestion`}>
-            <img alt={`${game.name} logo`} src={game.background_image} />
-            <div>
+      {data.results &&
+        data.results.map((game) => (
+          <ListGroupItem
+            type="button"
+            onClick={() => handleListItemClick(game.slug)}
+            key={`${game.name}Suggestion`}
+          >
+            <div className="img_wrapper">
+              <img alt={`${game.name} logo`} src={game.background_image} />
+            </div>
+            <div className="text_wrapper">
               <span>{game.name}</span>
-              <span>
-                {game.publishers.map((publisher) => publisher.name).join(" ,")}
+              <span className="badge_wrapper">
+                {game.genres.map((genre) => (
+                  <Badge bg="secondary" key={`${genre.name}Badge`}>
+                    {genre.name}
+                  </Badge>
+                ))}
               </span>
             </div>
           </ListGroupItem>
-        ))
-      ) : (
-        <div>No data</div>
-      )}
+        ))}
+      <div className="result-text_row">
+        <span>Results: {data.count || "-"}</span>
+        {data.count && data.count > 4 ? (
+          <Button>
+            More <ChevronRight />
+          </Button>
+        ) : (
+          <div />
+        )}
+      </div>
     </ListGroup>
   );
 }
