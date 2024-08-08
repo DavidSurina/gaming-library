a1be334b4413aed91623bc0fd6ef78fc396f719a
daveS <david.surina22@gmail.com>
2024-08-08 19:30:25 +0200

    base for the carousel on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect, useRef } from "react";
-import Container from "react-bootstrap/Container";
+import React, { useEffect, useRef, useState } from "react";
+import { Carousel, Container, Image } from "react-bootstrap";
+import { useQuery } from "@tanstack/react-query";
 
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import useRssFeed from "../../globals/hooks/useRssFeed";
 import NewsTile from "../../components/NewsTile/NewsTile";
 
+import { RawgApiService } from "../../globals/functions/rawgApi";
+import { rawgParams } from "../../globals/constants/rawgParams";
+import { GamesResultsType } from "../../globals/types/rawgTypes";
 import "./style.scss";
 
 function Home() {
   const { data, isLoading, fetchNextPage, isFetching, hasNextPage } =
     useRssFeed();
+  const { getRawgData } = RawgApiService;
+
+  const [carouselIndex, setCarouselIndex] = useState(0);
   const colEndRef = useRef(null);
+
+  const {
+    data: upcomingGamesData,
+    isLoading: upcomingGamesLoading,
+    error,
+  } = useQuery<GamesResultsType>({
+    queryKey: ["upcoming-games"],
+    queryFn: () => getRawgData(`/games?${rawgParams["Upcoming Games"]}`),
+  });
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -38,7 +54,30 @@ function Home() {
 
   return (
     <Container fluid="true" className="home-container">
-      <section className="news-container">
+      <h2>Upcoming games</h2>
+      <section className="carousel-section">
+        <Carousel
+          fade
+          activeIndex={carouselIndex}
+          interval={2000}
+          onSelect={(newIndex: number) => setCarouselIndex(newIndex)}
+          controls={false}
+          className="carousel-container"
+        >
+          {" "}
+          {upcomingGamesData?.results.map((item) => {
+            return (
+              <Carousel.Item key={`carouselItem${item.id}`}>
+                <div>
+                  <Image src={item.background_image} />
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      </section>
+      <h2>News</h2>
+      <section className="news-section">
         {data?.pages &&
           data.pages.map((pages) => {
             return pages.feedItems.map((item) => {
