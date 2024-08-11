930cc8db49dd6699d3c69f636d5a194071d8ae18
daveS <david.surina22@gmail.com>
2024-08-11 13:25:10 +0200

    adjusted home page to have carousel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,10 +10,18 @@ import { RawgApiService } from "../../globals/functions/rawgApi";
 import { rawgParams } from "../../globals/constants/rawgParams";
 import { GamesResultsType } from "../../globals/types/rawgTypes";
 import "./style.scss";
+import { Link } from "react-router-dom";
+import { LIBRARY_ROUTE } from "../../MainRoute";
 
 function Home() {
-  const { data, isLoading, fetchNextPage, isFetching, hasNextPage } =
-    useRssFeed();
+  const {
+    data,
+    isLoading: isRssLoading,
+    isInitialLoading: isRssInitialLoading,
+    fetchNextPage,
+    isFetching,
+    hasNextPage,
+  } = useRssFeed();
   const { getRawgData } = RawgApiService;
 
   const [carouselIndex, setCarouselIndex] = useState(0);
@@ -21,12 +29,15 @@ function Home() {
 
   const {
     data: upcomingGamesData,
-    isLoading: upcomingGamesLoading,
-    error,
+    isLoading: isUpcomingGamesLoading,
+    isInitialLoading: isUpcomingGamesInitialLoading,
   } = useQuery<GamesResultsType>({
-    queryKey: ["upcoming-games"],
-    queryFn: () => getRawgData(`/games?${rawgParams["Upcoming Games"]}`),
+    queryKey: ["upcoming-games-home"],
+    queryFn: () =>
+      getRawgData(`/games?page_size=6${rawgParams["Upcoming Games"]}`),
   });
+
+  const isInitialLoading = isRssInitialLoading || isUpcomingGamesInitialLoading;
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -50,48 +61,67 @@ function Home() {
     return () => {
       observer.disconnect();
     };
-  }, [isLoading, hasNextPage]);
+  }, [isRssLoading, hasNextPage]);
 
   return (
     <Container fluid="true" className="home-container">
-      <h2>Upcoming games</h2>
-      <section className="carousel-section">
-        <Carousel
-          fade
-          activeIndex={carouselIndex}
-          interval={2000}
-          onSelect={(newIndex: number) => setCarouselIndex(newIndex)}
-          controls={false}
-          className="carousel-container"
-        >
-          {" "}
-          {upcomingGamesData?.results.map((item) => {
-            return (
-              <Carousel.Item key={`carouselItem${item.id}`}>
-                <div>
-                  <Image src={item.background_image} />
-                </div>
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
-      </section>
-      <h2>News</h2>
-      <section className="news-section">
-        {data?.pages &&
-          data.pages.map((pages) => {
-            return pages.feedItems.map((item) => {
-              return <NewsTile key={`${item.title}tile`} data={item} />;
-            });
-          })}
-      </section>
-      {isLoading ||
-        (isFetching && (
-          <div className="loading-spacing">
-            <LoadingSpinner />
-          </div>
-        ))}
-      {data && <span ref={colEndRef} />}
+      {isInitialLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <section className="carousel-section">
+            {isUpcomingGamesLoading && !isUpcomingGamesInitialLoading ? (
+              <LoadingSpinner />
+            ) : (
+              <Carousel
+                fade
+                activeIndex={carouselIndex}
+                interval={2000}
+                onSelect={(newIndex: number) => setCarouselIndex(newIndex)}
+                controls={false}
+                className="carousel-container"
+              >
+                {" "}
+                {upcomingGamesData?.results.map((item) => {
+                  return (
+                    <Carousel.Item key={`carouselItem${item.id}`}>
+                      <Link to={`${LIBRARY_ROUTE}/${item.slug}`}>
+                        <Carousel.Caption className="carousel-caption_release">
+                          <span>{`Release: ${item.released || "-"}`}</span>
+                        </Carousel.Caption>
+                        <Carousel.Caption className="carousel-caption_upcoming">
+                          <h4>Upcoming games</h4>
+                        </Carousel.Caption>
+                        <Carousel.Caption className="carousel-caption_name">
+                          <h3>{item.name}</h3>
+                        </Carousel.Caption>
+                        <div>
+                          <Image src={item.background_image} />
+                        </div>
+                      </Link>
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+            )}
+          </section>
+          <h1>IGN News</h1>
+          <section className="news-section">
+            {data?.pages &&
+              data.pages.map((pages) => {
+                return pages.feedItems.map((item) => {
+                  return <NewsTile key={`${item.title}tile`} data={item} />;
+                });
+              })}
+          </section>
+          {(isRssLoading || isFetching) && !isRssInitialLoading && (
+            <div className="loading-spacing">
+              <LoadingSpinner />
+            </div>
+          )}
+          {data && <span ref={colEndRef} />}
+        </>
+      )}
     </Container>
   );
 }
